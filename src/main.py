6bbe0cf79be05e34ecaee231d5c386767bea1f79
Manuel <manuelmaterazzo@gmail.com>
2024-11-03 14:43:01 +0100

    train code

--- src/main.py
+++ src/main.py
@@ -1,48 +1,40 @@
 import pandas as pd
 
-from src.pipelines.housing_prices_competition_dt_pipeline import HousingPricesCompetitionDTPipeline
+from src.pipelines.brist1d_blood_glucose_prediction_dt_pipeline import BrisT1DBloodGlucosePredictionDTPipeline
 from src.trainer import Trainer
 from src.hyperparameter_optimizers.accurate_grid_optimizer import AccurateGridOptimizer
+from src.hyperparameter_optimizers.balanced_grid_optimizer import BalancedGridOptimizer
+from src.hyperparameter_optimizers.fast_bayesian_optimizer import FastBayesianOptimizer
 
 
 def load_data():
     # Load the data
-    iowa_file_path = '../resources/train.csv'
-    home_data = pd.read_csv(iowa_file_path)
+    file_path = '../resources/brist1d/train.csv'
+    data = pd.read_csv(file_path)
 
     # Remove rows with missing target, separate target from predictors
-    pruned_home_data = home_data.dropna(axis=0, subset=['SalePrice'])
-    y = pruned_home_data.SalePrice
-    X = pruned_home_data.drop(['SalePrice'], axis=1)
+    data.dropna(axis=0, subset=['bg+1:00'])
+    y = data['bg+1:00']
+    X = data.drop(['bg+1:00'], axis=1)
     return X, y
 
 
 X, y = load_data()
 
-pipeline = HousingPricesCompetitionDTPipeline(X, True)
+pipeline = BrisT1DBloodGlucosePredictionDTPipeline(X, True)
 
 trainer = Trainer(pipeline)
 
-params = {
-    'n_jobs': -1,  # Use all available cores
-    'objective': 'reg:squarederror',
-    'learning_rate': 0.03,
-}
-
-optimizer = AccurateGridOptimizer(trainer)
-
 # optimize parameters
+optimizer = FastBayesianOptimizer(trainer)
 optimized_params = optimizer.tune(X, y, 0.03)
 
-# Train model baseline
-# print()
-# print("Before optimization:")
-# _, boost_rounds = trainer.cross_validation(X, y, log_level=1, **params)
-# print()
+print("params")
+print(optimized_params)
 
-print("After optimization:")
+print()
 _, boost_rounds = trainer.cross_validation(X, y, log_level=1, **optimized_params)
 print()
 
 # fit complete_model on all data from the training data
-complete_model = trainer.train_model(X, y, rounds=boost_rounds, **params)
+complete_model = trainer.train_model(X, y, rounds=boost_rounds, **optimized_params)
