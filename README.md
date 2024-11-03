c683c192f787b010e556646a7c23eb2e51e6eebc
Manuel <manuelmaterazzo@gmail.com>
2024-11-03 21:24:16 +0100

    readme update

---
+++ README.md
@@ -0,0 +1,20 @@
+# Blood Glucose Prediction
+
+## Problem Statement
+Predicting **blood glucose fluctuations** is crucial for managing **type 1 diabetes**.\
+Effective algorithms can alleviate some of the challenges faced by individuals with this condition.\
+The primary goal of this project is to **forecast blood glucose levels** one hour ahead using the previous six hours of 
+participant data collected using **T1D devices** and **smartwatches**.
+
+## Dataset Description
+The data used in this competition is part of a newly collected dataset of real-world data collected from young adults in the UK who have type 1 diabetes.\
+All participants used continuous glucose monitors, and insulin pumps and were given a smartwatch as part of the study to collect activity data. 
+
+The data was collected by the [Jean Golding Institute](https://www.bristol.ac.uk/golding/), the complete dataset will be published after the competition for research purposes.\
+Some more details about the study can be found in [this blog post](https://jeangoldinginstitute.blogs.bristol.ac.uk/2024/08/19/how-smartwatches-could-help-people-with-type-1-diabetes/).
+
+### Synthetic Dataset
+The dataset used for the competition is private and its redistribution is prohibited.\
+For demonstration purposes, a synthetic dataset mimicking the original data's structure and distributions is provided.\
+You can find it at `resources/synthetic_dataset.csv`.
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 from src.pipelines.brist1d_blood_glucose_prediction_dt_pipeline import BrisT1DBloodGlucosePredictionDTPipeline
-from src.trainer import Trainer
+
 from src.hyperparameter_optimizers.accurate_grid_optimizer import AccurateGridOptimizer
 from src.hyperparameter_optimizers.balanced_grid_optimizer import BalancedGridOptimizer
 from src.hyperparameter_optimizers.fast_bayesian_optimizer import FastBayesianOptimizer
+from src.hyperparameter_optimizers.optuna_optimizer import OptunaOptimizer
+
+from src.trainers.accurate_cross_trainer import AccurateCrossTrainer
 
 
 def load_data():
@@ -23,17 +26,21 @@ X, y = load_data()
 
 pipeline = BrisT1DBloodGlucosePredictionDTPipeline(X, True)
 
-trainer = Trainer(pipeline)
+trainer = AccurateCrossTrainer(pipeline)
+
+# Create a cached trainer for the optimizer in order to speed up the optimization process
+# optimizer_trainer = CachedAccurateCrossTrainer(pipeline, X, y)
+# optimizer = OptunaOptimizer(optimizer_trainer)
 
 # optimize parameters
-optimizer = FastBayesianOptimizer(trainer)
+optimizer = BalancedGridOptimizer(trainer)
 optimized_params = optimizer.tune(X, y, 0.03)
 
 print("params")
 print(optimized_params)
 
 print()
-_, boost_rounds = trainer.cross_validation(X, y, log_level=1, **optimized_params)
+_, boost_rounds = trainer.validate_model(X, y, log_level=1, **optimized_params)
 print()
 
 # fit complete_model on all data from the training data
