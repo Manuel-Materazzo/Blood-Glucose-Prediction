b27553a4906fd55fbf05d3f3b8fc116bac7cbf51
Manuel <manuelmaterazzo@gmail.com>
2025-01-04 15:02:57 +0100

    main update

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
-import numpy as np
+import re
+
 import pandas as pd
-from sklearn.model_selection import cross_val_score
-from xgboost import DMatrix, XGBRegressor
 
 from src.enums.accuracy_metric import AccuracyMetric
 
 from src.models.xgb_regressor import XGBRegressorWrapper
-from src.hyperparameter_optimizers.accurate_grid_optimizer import AccurateGridOptimizer
-from src.hyperparameter_optimizers.balanced_grid_optimizer import BalancedGridOptimizer
-from src.hyperparameter_optimizers.fast_bayesian_optimizer import FastBayesianOptimizer
-from src.hyperparameter_optimizers.optuna_optimizer import OptunaOptimizer
 from src.pipelines.dt_pipeline import save_data_model
 from src.pipelines.empty_dt_pipeline import EmptyDTPipeline
 from src.preprocessors.blood_glucose_prediction_data_preprocessor import BloodGlucoseDataPreprocessor
@@ -17,38 +12,53 @@ from src.preprocessors.blood_glucose_prediction_data_preprocessor import BloodGl
 from src.trainers.accurate_cross_trainer import AccurateCrossTrainer
 from src.trainers.trainer import save_model
 
+
 def load_data():
     # Load the data
-    file_path = '../resources/brist1d/train.csv'
+    file_path = '/kaggle/input/brist1d/train.csv'
     data = pd.read_csv(file_path, index_col='id')
 
     # Remove rows with missing target, separate target from predictors
-    data.dropna(axis=0, subset=['bg+1:00'])
-    y = data['bg+1:00']
-    X = data.drop(['bg+1:00'], axis=1)
-    X = X.drop(['p_num'], axis=1)
-    return X, y
+    data.dropna(axis=0, subset=['bg+1:00'], inplace=True)
+
+    # rename target column
+    data['target'] = data['bg+1:00']
+    data = data.drop(['bg+1:00'], axis=1)
+
+    # standardize column names
+    data = data.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '_', x))
+
+    return data
+
 
 print("Loading data...")
-X, y = load_data()
+data = load_data()
+
+# instantiate data preprocessor
+preprocessor = BloodGlucoseDataPreprocessor()
+
+# preprocess train data
+# different from preprocess_data because of the data increase opportunity
+print("Preprocessing data...")
+data = preprocessor.preprocess_train_data(data)
+
+# get X and y from preprocessed data
+y = data['target']
+X = data.drop(['target'], axis=1)
+del data  # free memory
 
 # save model file for current dataset on target directory
 print("Saving data model...")
 save_data_model(X)
 
-# instantiate data pipeline and preprocessor
-preprocessor = BloodGlucoseDataPreprocessor()
+# instantiate data pipeline
 pipeline = EmptyDTPipeline(X)
 
-# preprocess train data
-# different from preprocess_data because of the data increase opportunity
-preprocessor.preprocess_train_data(X)
-
 # pick a model, and a trainer
-model_type = XGBRegressorWrapper()
+model_type = XGBRegressorWrapper(early_stopping_rounds=250)
 trainer = AccurateCrossTrainer(pipeline, model_type, AccuracyMetric.RMSE, grouping_columns=['p_num'])
 
-# optimizing parameters worsens performance
+# optimizing parameters worsens performance and takes too much time
 optimized_params = {
     'objective': 'reg:squarederror',
     'learning_rate': 0.03,
